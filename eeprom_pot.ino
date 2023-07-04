5b77586f002d94c74717e6be17427d1924c0f067
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2023-07-04 15:49:38 +0300

    added test code

--- eeprom_pot.ino
+++ eeprom_pot.ino
@@ -1,11 +1,3 @@
-# EEPROME_IoT
-Flow diagram
-
-![eeprom drawio](https://github.com/AronAyub/EEPROM_IoT/assets/55284959/fe52f056-68ad-4e9d-bd1a-e8c944508a68)
-
-- **EEPROM- test with POT.**
-
-```
 /*
   External EEPROM Recording & Playback Demo
   ext_eeprom_demo.ino
@@ -123,7 +115,3 @@ void loop()
 
   // Nothing in loop
 }
-```
-
-
- Store and transmit data Over, Sigfox and Wifi
