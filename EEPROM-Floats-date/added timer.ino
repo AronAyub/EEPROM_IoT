3d899158a14b2ef287a1de65bd2f6a26a629ff41
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2023-10-08 15:34:47 +0300

    added timer

--- EEPROM-Floats-date/added timer.ino
+++ EEPROM-Floats-date/added timer.ino
@@ -1,3 +1,4 @@
+#include "Adafruit_ZeroTimer.h"
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
@@ -10,11 +11,36 @@
 DHT dht(DHT_PIN, DHT_TYPE);
 RTC_DS3231 rtc;
 
+volatile int alarm_source = 0;  //variable for sleep mode
+volatile int Timer_seconds=0;
+volatile int Read_store=0;
+float freq = 1; // 1 KHz
+Adafruit_ZeroTimer zerotimer = Adafruit_ZeroTimer(3);
+
+
 struct TemperatureReading {
   int address;
   float temperature;
   DateTime timestamp;
 };
+
+void TC3_Handler() {
+  Adafruit_ZeroTimer::timerHandler(3);
+}
+
+volatile bool togglepin = false;
+void TimerCallback0(void)
+{
+  if (Timer_seconds < 30)
+  {
+    Timer_seconds++;
+  }
+  else
+  {
+    Read_store=1;
+    Timer_seconds = 0;
+  }
+}
 
 int numReadings = 0; // Track the number of readings
 
@@ -33,18 +59,75 @@ void setup() {
     Serial.println("RTC lost power, let's set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
-
-
   
-
   while (!Serial) {
     // Wait for the serial monitor to open
   }
+
+   // Set up the flexible divider/compare
+  uint8_t divider  = 1;
+  uint16_t compare = 0;
+  tc_clock_prescaler prescaler = TC_CLOCK_PRESCALER_DIV1;
+
+  if ((freq < 24000000) && (freq > 800)) {
+    divider = 1;
+    prescaler = TC_CLOCK_PRESCALER_DIV1;
+    compare = 48000000 / freq;
+  } else if (freq > 400) {
+    divider = 2;
+    prescaler = TC_CLOCK_PRESCALER_DIV2;
+    compare = (48000000 / 2) / freq;
+  } else if (freq > 200) {
+    divider = 4;
+    prescaler = TC_CLOCK_PRESCALER_DIV4;
+    compare = (48000000 / 4) / freq;
+  } else if (freq > 100) {
+    divider = 8;
+    prescaler = TC_CLOCK_PRESCALER_DIV8;
+    compare = (48000000 / 8) / freq;
+  } else if (freq > 50) {
+    divider = 16;
+    prescaler = TC_CLOCK_PRESCALER_DIV16;
+    compare = (48000000 / 16) / freq;
+  } else if (freq > 12) {
+    divider = 64;
+    prescaler = TC_CLOCK_PRESCALER_DIV64;
+    compare = (48000000 / 64) / freq;
+  } else if (freq > 3) {
+    divider = 256;
+    prescaler = TC_CLOCK_PRESCALER_DIV256;
+    compare = (48000000 / 256) / freq;
+  }
+  else if (freq >= 0.75)
+  {
+    divider = 1024;
+    prescaler = TC_CLOCK_PRESCALER_DIV1024;
+    compare = (48000000 / 1024) / freq;
+  }
+  else
+  {
+    Serial.println("Invalid frequency");
+    while (1) delay(10);
+  }
+  Serial.print("Divider:"); Serial.println(divider);
+  Serial.print("Compare:"); Serial.println(compare);
+  Serial.print("Final freq:"); Serial.println((int)(48000000 / compare));
+
+  zerotimer.enable(false);
+  zerotimer.configure(prescaler,       // prescaler
+                      TC_COUNTER_SIZE_16BIT,       // bit width of timer/counter
+                      TC_WAVE_GENERATION_MATCH_PWM // frequency or PWM mode
+                     );
+
+  zerotimer.setCompare(0, compare);
+  zerotimer.setCallback(true, TC_CALLBACK_CC_CHANNEL0, TimerCallback0);
+  zerotimer.enable(true);
 }
 
+
 void writeTemperatureToEEPROM(int address, float temperature, DateTime timestamp) {
-  byte* p = (byte*)(void*)&temperature;
-  for (int i = 0; i < sizeof(temperature); i++) {
+  byte* p = (byte*)(void*)&temperature; // pointer p uses type casting to treat tmp as series of byts, why ? EEPROM data is stored as byte
+  for (int i = 0; i < sizeof(temperature); i++) {//iterate through suize of temp
     Wire.beginTransmission(EEPROM_ADDRESS);
     Wire.write((address + i) >> 8); // High byte of address
     Wire.write((address + i) & 0xFF); // Low byte of address
@@ -64,14 +147,14 @@ void writeTemperatureToEEPROM(int address, float temperature, DateTime timestamp
   p = (byte*)(void*)&unixTime;
   for (int i = 0; i < sizeof(unixTime); i++) {
     Wire.beginTransmission(EEPROM_ADDRESS);
-    Wire.write((address + sizeof(temperature) + i) >> 8); // High byte of address
+    Wire.write((address + sizeof(temperature) + i) >> 8); // High byte of address, ensures we store time excatly after temp value
     Wire.write((address + sizeof(temperature) + i) & 0xFF); // Low byte of address
     Wire.write(p[i]); // Write each byte of the Unix time
     Wire.endTransmission();
     delay(5); // Delay to ensure writes are complete
   }
 }
-
+//read temp and timestamp
 void readTemperatureFromEEPROM(int address, TemperatureReading& reading) {
   //read Temperature floating points 
 
@@ -138,25 +221,37 @@ void printTemperatureReading(const TemperatureReading& reading) {
   Serial.print("Data Size (bytes): ");
   int dataSize = sizeof(float) + sizeof(unsigned long);
   Serial.println(dataSize);
-  Serial.println("Next value afte 10 sec: ");
+  Serial.println("Next value afte 30 sec: ");
   
 }
 
 void loop() {
+
+  if (Read_store==1)
+  {
   float temperature = dht.readTemperature();
   DateTime now = rtc.now();
   TemperatureReading reading;
-  reading.address = numReadings * (sizeof(float) + sizeof(unsigned long)); // Calculate address
-  reading.temperature = temperature;
+  reading.address = numReadings * (sizeof(float) + sizeof(unsigned long)); // Calculate address to be used based on float and time size
+  reading.temperature = temperature; // assigns temp to temp field of the struct  
   reading.timestamp = now;
 
-  writeTemperatureToEEPROM(reading.address, reading.temperature, reading.timestamp);
+  writeTemperatureToEEPROM(reading.address, reading.temperature, reading.timestamp); //writting temp and timestamp at calculated addresses
 
   // Increment the number of readings
   numReadings++;
 
   // Print the memory address, temperature, and timestamp
   printTemperatureReading(reading);
-
-  delay(10000);
+  Read_store=0;
+  }
+  
+else
+  {
+    Serial.print("TIMER:"); 
+    Serial.print(Timer_seconds);
+    Serial.println(" S");
+    delay(1000);
+    }
+  
 }
